648f6ef34edbcb7aa24658540acc942b1d046ad1
8bitme2 <jeffry.herzog.187@gmail.com>
2023-07-09 11:53:01 -0400

    Menu Transition

---
+++ Code/StateTrigger.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateTrigger : MonoBehaviour
+{
+    public void Transition(string name)
+    {
+        ManageScene.Instance.LoadScene(name);
+    }
+}
